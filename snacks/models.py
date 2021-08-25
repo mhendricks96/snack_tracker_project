6a6be3d11c1d9a50d9a3c79415cd7e92cf134c34
mhendricks96 <mhendricks96@yahoo.com>
2021-08-25 15:39:41 -0700

    finished setting up db in admin, need to make visible to users

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Snack(models.Model):
+  name = models.CharField(max_length=64)
+  description = models.TextField(max_length=300)
+  purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
