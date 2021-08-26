4a2de7596576e96fc70f68654178b8d137330fea
mhendricks96 <mhendricks96@yahoo.com>
2021-08-25 18:56:08 -0700

    done with project, now needs tests

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import HomeView, SnackListView
+from .views import SnackListView, HomeView, SnackDetailView
 
 urlpatterns=[
     path('', HomeView.as_view(), name='home'),
-    path('snack_list', SnackListView.as_view(), name='snack_list'),
+    path('snacks/', SnackListView.as_view(), name='snack_list'),
+    path('<int:pk>', SnackDetailView.as_view(), name='snack_detail')
 ]
